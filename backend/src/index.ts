71bbc3bf5e7763950101eeb025d909798d947720
Nalwa-Jayesh <64242951+Nalwa-Jayesh@users.noreply.github.com>
2025-02-12 22:23:11 +0530

    db changes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,10 @@
 import express from 'express';
 import authRouter from "./routes/auth";
 import taskRouter from "./routes/tasks";
-import cors from 'cors';
 
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 
-app.use(cors());
 app.use(express.json());
 app.use('/auth', authRouter);
 app.use('/tasks', taskRouter)

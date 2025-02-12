464dd822fc06ed95c49da3917b38f4610e83b52a
Nalwa-Jayesh <64242951+Nalwa-Jayesh@users.noreply.github.com>
2025-02-12 22:29:02 +0530

    db changes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,18 +1,18 @@
 import express from 'express';
 import authRouter from "./routes/auth";
 import taskRouter from "./routes/tasks";
+import cors from "cors";
 
 const app = express();
-const port = process.env.PORT || 8000;
-
+app.use(cors());
 app.use(express.json());
 app.use('/auth', authRouter);
 app.use('/tasks', taskRouter)
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
     res.send("Welcome to my app!!!");
 })
 
-app.listen(port, () => {
+app.listen(8000, () => {
     console.log("Server running on port 8000");
 });
